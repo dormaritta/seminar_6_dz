1d221053479f46c0f8ab14f31b288eecb611b0ab
Dara Kosova <darakosova.gb@gmail.com>
2022-07-17 08:06:20 +0400

    Ввела проверку на совпадение прямых и парал-ть

--- task_43/Program.cs
+++ task_43/Program.cs
@@ -3,19 +3,23 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
 
 Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
  
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
- 
-Console.WriteLine($"Пересечение в точке: ({x}; {y})");
+if (k1 == k2)
+{
+    if (k1 == k2 && b1 == b2) Console.WriteLine($"Прямые совпадают");
+    else Console.WriteLine($"Прямые параллельны");
+}
+else
+{
+    double x = (-b2 + b1) / (-k1 + k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"Пересечение в точке: ({x}; {y})"); 
+}
+
